8d696849ae6e269b7e3113a249b5e020d0177178
Dorian Chau <dorian.chau@gmail.com>
2020-12-06 08:28:27 -0500

    Fix pipeline

--- web/backend/app/core/pipeline.py
+++ web/backend/app/core/pipeline.py
@@ -39,8 +39,7 @@ def method3(input_paths, filenames, feature_path):
         centroids, labels = generate_clusters(imgs_features, n_clusters=5)
         clust = get_closest_cluster(input_path, centroids)
         indexes = np.where(labels == clust)[0]
-        duplicates, _ = search_duplicates_hash(
-            input_path, indexes, imgs_features)
+        duplicates, _ = search_duplicates_orb(input_path, indexes, filenames)
         outputs += duplicates
     outputs = set(outputs)
     return [filenames[output] for output in outputs]
